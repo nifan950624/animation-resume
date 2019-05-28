86d029123210e37ff255e7a69744cf5379c1b11b
nifan <nifan1950333426@qq.com>
2019-05-28 23:37:45 +0800

    转化成Markdoen

--- js/main.js
+++ js/main.js
@@ -12,8 +12,9 @@ var result = `
 
   /*我们背景太单调了，给背景一个颜色吧*/
   body{
-    background-color: rgb(217,217,217); 
-    padding: 30px;
+    padding: 30px;  
+    color: white;
+    background-color: rgb(20,60,60); 
   } 
 
   #code{
@@ -21,42 +22,50 @@ var result = `
   }
 
   /*下面給字体来些高亮，让您看上去更舒服些*/
-  .token.function{
-    color: #DD4A68
-  }
-
-  .token.property{
-    color: #905
+  .token.selector{
+    color: #690;
   }
 
   .token.punctuation{
     color: #999
   }
 
-  .token.selector{
-    color: #690;
+  .token.property{
+    color: rgb(157,122,9)
+  }
+
+  .token.function{
+    color: #DD4A68
   }
 
   /*给它加点特效吧*/
+  body{
+    perspective: 1000px;
+  }
+
   #code{
     animation: breath 0.5s infinite alternate-reverse;
+    transform: rotateY(15deg) translateZ(-100px) ;
   } 
   `
-  var result2 = `/*下面开始写我的简历，首先我需要一张白纸让它在页面的右边*/
+var result2 = `
+  /*下面开始写我的简历，首先我需要一张白纸让它在页面的右边*/
   #code{
     width: 50%;
-    height: 100vh;
+    height: 100%;
     margin-right: 30px;
   }
 
   #paper{
-    height: 100vh;
+    height: 100%;
     width: 50%;
     background:white;
   }
+  
+  /*好了我可以在上面写字了*/
   `
 
-  var md = `
+var md = `
   我叫倪凡
 
   年龄：24
@@ -71,7 +80,32 @@ var result = `
       3.简历
   `
 
-function markDown(md) {
+var result3 = `
+  /*
+  *喔喔，好像还差点什么
+  *这个是markdown呀
+  *下面我将使用marked的库将我的
+  *简历转化成HTML格式
+  */
+  `
+
+var result4 = `
+  /*
+  *已经转化成功了，下面来给他写些样式*/
+  #paper>div{
+    padding:20px;
+  }
+
+  #paper>div>p,
+  #paper>div>h1,
+  #paper>div ol li,
+  #paper>div ol h3{
+    padding:10px 0
+  }
+  `
+
+
+function markDown(md, fn) {
   var pre = document.createElement('pre')
   paper.appendChild(pre)
   var content = document.querySelector('#paper>pre')
@@ -82,9 +116,9 @@ function markDown(md) {
     content.scrollTop = content.scrollHeight;
     if (n >= md.length) {
       window.clearInterval(id)
-
+      fn.call()
     }
-  }, 0)
+  }, 50)
 }
 
 
@@ -101,14 +135,21 @@ function writeCode(preResult, result, fn) {
       window.clearInterval(id)
       fn.call()
     }
-  }, 0)
+  }, 50)
 }
 
 
 writeCode('', result, () => {
   createPaper()
   writeCode(result, result2, () => {
-    markDown(md)
+    markDown(md, () => {
+      writeCode(result + result2, result3, () => {
+        markdownToHTML()
+        writeCode(result + result2 + result3, result4, () => {
+
+        })
+      })
+    })
   })
 })
 
@@ -117,4 +158,26 @@ function createPaper() {
   var paper = document.createElement('div')
   document.body.appendChild(paper)
   paper.id = 'paper';
+}
+
+
+function markdownToHTML() {
+  var content = document.querySelector('#paper>pre')
+  div = document.createElement('div');
+  paper.replaceChild(div, content)
+  paper.appendChild(div);
+  div.innerHTML = marked(`
+  <h1>我叫倪凡</h1>
+  <p>年龄：24</p>
+  <p>来自武汉纺织大学</p>
+  <p>自学前端半年</p>
+  <hr style>
+  <ol>
+  <h3>主要项目:</h3> 
+      <li><a href="https://nifan950624.github.io/kbd-demo/" target="_blank">导航</a></li>
+      <li><a href="https://nifan950624.github.io/canvas-demo/" target="_blank">个人画板</a></li>
+      <li><a href="" target="_blank">简历></a></li>
+  </ol>
+  `)
+
 }
